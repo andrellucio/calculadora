07686e9d095fd9a017539b2cd5e50d6a2b9cd2b7
André Lucio <43222862+andrellucio@users.noreply.github.com>
2023-03-31 13:33:52 -0300

    Create calculadora.h

---
+++ calculadora.h
@@ -0,0 +1,8 @@
+// Módulo de cálculo (calculadora.h)
+#ifndef CALCULADORA_H
+#define CALCULADORA_H
+int soma(int num1, int num2);
+int subtracao(int num1, int num2);
+int multiplicacao(int num1, int num2);
+int divisao(int num1, int num2);
+#endif
